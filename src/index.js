e9e364e77c07d89ff6ebb237a540c13e5510fcd1
Amanda093 <vovigadagamer@gmail.com>
2023-12-20 16:21:29 -0300

    projeto criado

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// CRIANDO UM SERVIDOR NODEJS EXPRESS
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/pages/home.html"));
+});
+
+router.get("/contato", (req, res) => {
+  res.sendFile(path.join(__dirname + "/pages/contato.html"));
+});
+
+app.use(router);
+
+app.listen(3333, () => {
+  console.log("Servidor rodando...");
+});
+/*
+// CRIANDO UM SERVIDOR NODEJS VANILLA HTTP
+const http = require("http");
+
+const hostname = "localhost";
+const port = 3333;
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log("Servidor rodando...");
+});
+*/
